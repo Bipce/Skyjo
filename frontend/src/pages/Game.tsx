ffdb4b8f7e4afa9c339652387ebc8ca6b0c94b9e
Claire Royer <claireroyer57@gmail.com>
2026-04-11 22:57:37 +0200

    feat: implement Game page

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -1,12 +1,24 @@
-import { useNavigate } from "react-router";
+import PlayerPanel from "../components/PlayerPanel.tsx";
+import Wrapper from "../components/Wrapper.tsx";
+import CartDeck from "../components/CartDeck.tsx";
 
 const Game = () => {
-  const navigate = useNavigate();
-
   return (
-    <div>
-      <button onClick={() => navigate("/")}>Aller a l'accueil</button>
-    </div>
+    <main className="center h-full min-h-screen w-full flex-col gap-10 p-5">
+      <div className="flex min-w-full gap-10">
+        <Wrapper isYours={false}>
+          <PlayerPanel isYours={false} />
+        </Wrapper>
+        <div className="flex flex-col gap-5">
+          <CartDeck>?</CartDeck>
+          <CartDeck>9</CartDeck>
+        </div>
+      </div>
+
+      <Wrapper isYours={true}>
+        <PlayerPanel isYours={true} />
+      </Wrapper>
+    </main>
   );
 };
 
