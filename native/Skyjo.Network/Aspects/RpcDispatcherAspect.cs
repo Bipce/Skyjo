1a52f8e38f07fdfa27843f2a2d46cdf5e1423354
Damien Royer <royerdamien57@gmail.com>
2026-04-14 13:44:58 +0200

    [Network] Simplify RpcDispatcherAspect

--- native/Skyjo.Network/Aspects/RpcDispatcherAspect.cs
+++ native/Skyjo.Network/Aspects/RpcDispatcherAspect.cs
@@ -1,61 +1,33 @@
 using LiteNetLib.Utils;
 using Metalama.Framework.Aspects;
 using Metalama.Framework.Code;
-using Metalama.Framework.Code.SyntaxBuilders;
 using Skyjo.Network.Utils;
 
 namespace Skyjo.Network.Aspects;
 
 internal sealed class RpcDispatcherAspect : TypeAspect
 {
-    [Template] private static Dictionary<int, Action<Entity, NetDataReader>>? _rpcHandlers;
+    [Introduce] private static Dictionary<int, Action<NetDataReader>>? _rpcHandlers;
 
-    public override void BuildAspect(IAspectBuilder<INamedType> builder)
+    [Introduce(Accessibility = Accessibility.Protected, WhenExists = OverrideStrategy.Override)]
+    private void InternalCallMethod(int id, NetDataReader reader)
     {
-        var cases = builder.Target.Methods
-            .Where(m => m.Attributes.Any(a => a.Type.IsConvertibleTo(typeof(RpcMethodAspect))))
-            .Select(m =>
-            {
-                var args = string.Join(", ", m.Parameters
-                    .Select(p => NetworkHelper.GetReaderExpression(p.Type).Replace("reader.", "r.")));
-                var lambda = $"(Skyjo.Network.Entity e, LiteNetLib.Utils.NetDataReader r) => " +
-                             $"(({m.DeclaringType.FullName})e).{m.Name}({args})";
-                return (Id: NetworkHelper.ComputeMethodId(m), LambdaString: lambda);
-            })
-            .ToList();
+        var methods = meta.Target.Type.Methods
+            .Where(x => x.Attributes.Any(a => a.Type.IsConvertibleTo(typeof(RpcMethodAspect)))).ToArray();
 
-        var fieldResult =
-            builder.IntroduceField(nameof(_rpcHandlers), IntroductionScope.Static, OverrideStrategy.Ignore);
-
-        builder.IntroduceMethod(
-            nameof(InternalCallMethodTemplate),
-            IntroductionScope.Instance,
-            OverrideStrategy.Override,
-            b =>
-            {
-                b.Name = "InternalCallMethod";
-                b.Accessibility = Accessibility.Protected;
-            },
-            args: new { cases, handlersField = fieldResult.Declaration });
-    }
-
-    [Template]
-    internal void InternalCallMethodTemplate(
-        int id,
-        NetDataReader reader,
-        [CompileTime] List<(int Id, string LambdaString)> cases,
-        [CompileTime] IField handlersField)
-    {
-        if (handlersField.Value == null)
+        if (_rpcHandlers == null)
         {
-            handlersField.Value = ExpressionFactory.Parse(
-                meta.CompileTime(
-                    "new System.Collections.Generic.Dictionary<int, System.Action<Skyjo.Network.Entity, LiteNetLib.Utils.NetDataReader>>(" +
-                    cases.Count + ") { " +
-                    string.Join(", ", cases.Select(c => $"[{c.Id}] = {c.LambdaString}")) +
-                    " }"));
+            _rpcHandlers = new Dictionary<int, Action<NetDataReader>>(methods.Length);
+            foreach (var method in methods)
+            {
+                var methodId = NetworkHelper.ComputeMethodId(method);
+                var parameters = method.Parameters.Select(x => NetworkHelper.GetReaderExpression(x.Type));
+                var args = string.Join(", ", parameters);
+                var body = $"this.{method.Name}({args});";
+                meta.InsertStatement($"_rpcHandlers[{methodId}] = (reader) => {{{body}}};");
+            }
         }
 
-        ((Dictionary<int, Action<Entity, NetDataReader>>)handlersField.Value!)[id]((Entity)meta.This, reader);
+        _rpcHandlers[id](reader);
     }
 }
