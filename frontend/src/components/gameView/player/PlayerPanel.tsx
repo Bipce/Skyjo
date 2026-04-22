e2f1cf6e567b23bd79fa0ea83b6ce4e758739a4d
Claire Royer <claireroyer57@gmail.com>
2026-04-22 17:33:49 +0200

    refactor: change name of props

--- frontend/src/components/gameView/player/PlayerPanel.tsx
+++ frontend/src/components/gameView/player/PlayerPanel.tsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import ScorePanel from "../shared/score/ScorePanel.tsx";
-import CardGridWrapper from "../shared/card/CardGridWrapper.tsx";
-import type { CardData } from "../../../interfaces/CardData.ts";
 import Separation from "../../ui/Separation.tsx";
+import CardGridWrapper from "../shared/card/CardGridWrapper.tsx";
 import type { PlayerData } from "../../../interfaces/PlayerData.ts";
+import type { CardData } from "../../../interfaces/CardData.ts";
 
 interface Props {
   player: PlayerData;
@@ -10,12 +11,28 @@ interface Props {
 
 const PlayerPanel = ({ player }: Props) => {
   const cards: CardData[] = player.cards;
+  const [selectedCardsForInitiateGame, setSelectedCardsForInitiateGame] = useState<number[]>([]);
+
+  const handleSelectedForInitiateGame = (i: number) => {
+    setSelectedCardsForInitiateGame(prev => {
+      if (prev.includes(i)) return prev.filter(index => index !== i);
+      if (prev.length < 2) return [...prev, i];
+
+      return prev;
+    });
+  };
 
   return (
     <section className="relative flex h-full w-full justify-center">
       <Separation className="right-1/3" />
       <div className="panel-base h-full w-2/3 justify-around gap-10 p-8">
-        <CardGridWrapper cards={cards} className="w-full gap-10" belongsTo="player" />
+        <CardGridWrapper
+          cards={cards}
+          className="w-full gap-10"
+          belongsTo="player"
+          onSelectedForRevealCard={handleSelectedForInitiateGame}
+          selectedCardsForInitiateGame={selectedCardsForInitiateGame}
+        />
         <ScorePanel player={player} />
       </div>
     </section>
--- frontend/src/components/gameView/shared/card/CardGridWrapper.tsx
+++ frontend/src/components/gameView/shared/card/CardGridWrapper.tsx
@@ -5,13 +5,27 @@ interface Props {
   className?: string;
   cards: CardData[];
   belongsTo: CardBelongToType;
+  onSelectedForRevealCard: (index: number) => void;
+  selectedCardsForInitiateGame: number[];
 }
 
-const CardGridWrapper = ({ cards, belongsTo, className }: Props) => {
+const CardGridWrapper = ({
+  cards,
+  belongsTo,
+  selectedCardsForInitiateGame,
+  onSelectedForRevealCard,
+  className,
+}: Props) => {
   return (
     <div className={`grid grid-cols-4 ${className}`}>
       {cards.map((card, i) => (
-        <Card key={i} card={card} belongsTo={belongsTo} />
+        <Card
+          key={i}
+          card={card}
+          belongsTo={belongsTo}
+          handleOnClick={() => onSelectedForRevealCard(i)}
+          isSelected={selectedCardsForInitiateGame.includes(i)}
+        />
       ))}
     </div>
   );
